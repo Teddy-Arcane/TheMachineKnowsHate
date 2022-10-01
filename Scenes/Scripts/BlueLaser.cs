14f9083a07c2b6ef48dfd62f2a6e33d1e31ae337
Jonathon Reid <jonathon.reid@proactis.com>
2022-10-02 00:56:57 +0100

    Added blue lasers.

---
+++ Scenes/Scripts/BlueLaser.cs
@@ -0,0 +1,91 @@
+using Godot;
+using System;
+
+public class BlueLaser : RayCast2D
+{
+	private bool _isCasting;
+		private Tween _tween;
+		private Line2D _line;
+		private Player _player;
+		private LightFlicker _lightFlicker;
+	
+		public override void _Ready()
+		{
+			_tween = GetNode<Tween>("Tween");
+			_line = GetNode<Line2D>("Line2D");
+			_player = GetTree().Root.GetNode<Player>("GameWorld/Player");
+			_lightFlicker = GetTree().Root.GetNode<LightFlicker>("GameWorld/LightFlicker");
+
+			_lightFlicker.Connect("LightsToggled", this, "LightsToggled");
+			
+			SetPhysicsProcess(false);
+			_line.Points[1] = Vector2.Zero;
+			
+			SetIsCasting(true);
+		}
+	
+		public override void _PhysicsProcess(float delta)
+		{
+			var castPoint = CastTo;
+			ForceRaycastUpdate();
+			
+			if (IsColliding())
+			{
+				var hit = GetCollider();
+				var test = hit as KinematicBody2D;
+				if (test != null)
+					_player.Kill();
+				
+				castPoint = ToLocal(GetCollisionPoint());
+			}
+	
+			_line.SetPointPosition(1, castPoint);
+
+			Visible = LightFlicker.LightsOn;
+		}
+	
+		private void SetIsCasting(bool cast)
+		{
+			_isCasting = cast;
+	
+			if (_isCasting)
+			{
+				Appear();
+			}
+			else
+			{
+				Dissapear();
+			}
+			
+			SetPhysicsProcess(_isCasting);
+		}
+	
+		private void Appear()
+		{
+			_tween.StopAll();
+			_tween.InterpolateProperty(_line, "width", 0f, 2f, 0.1f);
+			_tween.Start();
+		}
+	
+		private void Dissapear()
+		{
+			_tween.StopAll();
+			_tween.InterpolateProperty(_line, "width", 10f, 0f, 0.1f);
+			_tween.Start();
+		}
+
+		private void LightsToggled(bool on)
+		{
+			if (on)
+			{
+				CollisionMask = 1;
+				Appear();
+				
+			}
+			else
+			{
+				CollisionMask = 10;
+				Dissapear();
+			}
+		}
+}
--- Scenes/Scripts/LightFlicker.cs
+++ Scenes/Scripts/LightFlicker.cs
@@ -3,39 +3,55 @@ using System;
 
 public class LightFlicker : CanvasModulate
 {
-	[Export] private bool _lightsOn = true;
 	[Export] private float _lerpRate = 0.05f;
 	
 	private Timer _timer;
+	private Timer _lightsOnTimer;
 	private MainUI _ui;
+
+	public static float TimeLeft = 0f;
+	public static bool LightsOn = false;
+	
+	[Signal] public delegate void LightsToggled(bool on);
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		_ui = GetTree().Root.GetNode<Control>("GameWorld/MainUI") as MainUI;
 		_timer = GetNode<Timer>("Timer");
+		_lightsOnTimer = GetNode<Timer>("LightsOnTimer");
+
+		_on_Timer_timeout();
 	}
 
 	public override void _Process(float delta)
 	{
-		_ui.SetHint(_timer.TimeLeft.ToString("0.0"));
+		TimeLeft = _timer.TimeLeft;
+		_ui.SetHint(TimeLeft.ToString("0.0"));
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
-		if (_lightsOn)
-			Color = Colors.White;
-		else
-		{
-			Color = Color.LinearInterpolate(Colors.Black, _lerpRate);
-		}
+		Color = Color.LinearInterpolate(Colors.Black, _lerpRate);
 	}
 	
 	private void _on_Timer_timeout()
 	{
 		Color = Colors.White;
+		LightsOn = true;
+		_lightsOnTimer.Start();
+		
+		EmitSignal("LightsToggled", true);
+	}
+	
+	private void _on_LightsOnTimer_timeout()
+	{
+		EmitSignal("LightsToggled", false);
 	}
 }
+
+
+
 
 
 
