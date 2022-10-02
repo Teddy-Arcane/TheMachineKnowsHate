2f7d6793bdf66aca6ac602e533dd7a13111e9d90
Jonathon Reid <jonathon.reid@proactis.com>
2022-10-02 21:40:32 +0100

    Added falling platform

---
+++ Scenes/Scripts/FallingPlatform.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public class FallingPlatform : KinematicBody2D
+{
+	private AudioPlayer _audioPlayer;
+	private Timer _fallTimer;
+	private AnimatedSprite _animated;
+	private CollisionShape2D _collider;
+	private bool _cracked = false;
+
+	public override void _Ready()
+	{
+		_audioPlayer = GetNode<AudioPlayer>("AudioPlayer");
+		_fallTimer = GetNode<Timer>("FallTimer");
+		_animated = GetNode<AnimatedSprite>("AnimatedSprite");
+		_collider = GetNode<CollisionShape2D>("CollisionShape2D");
+	}
+
+	public void Crack()
+	{
+		if (!_cracked)
+		{
+			_audioPlayer.Play("Crack");
+			_animated.Play("crack");
+			_fallTimer.Start();
+			_cracked = true;
+		}
+	}
+
+	private void _on_FallTimer_timeout()
+	{
+		_audioPlayer.Play("Break");
+		_collider.Disabled = true;
+		Visible = false;
+	}
+}
